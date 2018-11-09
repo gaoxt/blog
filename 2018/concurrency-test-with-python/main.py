d115989625c2f366e487551c3c183f3cb02ec106
gaoxt <simerpc@gmail.com>
2018-11-09 10:17:41 +0800

    debug

--- 2018/concurrency-test-with-python/main.py
+++ 2018/concurrency-test-with-python/main.py
@@ -1,7 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
-from multiprocessing import Process, Queue, Pool, Manager
+from multiprocessing import Process, Pool, Manager
 from threading import Thread
+import gevent
+from user_agent import generate_user_agent
+# from multiprocessing import Queue
+# gevent方式使用的Queue ，测试请注释上面的 Queue
+from gevent.queue import Queue
+
+headers = {
+    'User-Agent': generate_user_agent(),
+}
 
 max_page = 10
 cut_count = 5
@@ -15,8 +24,7 @@ process_data = [url_list[i:i + cut_count]
 
 def get_page_data(url):
     return url
-    response = requests.get(url, headers={
-                            'User-Agent': "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.2; Trident/4.0)"})
+    response = requests.get(url, headers)
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup.select("#content_left .c-container h3")[0].get_text().strip()
 
@@ -80,6 +88,72 @@ def test_thread():
     print(data)
 
 
+def test_coroutine():
+    q = Queue()
+    c = coroutine_consumer(q)
+    coroutine_produce(c)
+
+    print('total: %d' % q.qsize())
+    while not q.empty():
+        print(q.get())
+
+
+def coroutine_consumer(q):
+    while 1:
+        title = yield get_page_data
+        q.put(title)
+
+
+def coroutine_produce(c):
+    a = c.send(None)
+    for i in range(0, process_count):
+        [c.send(a(j)) for j in process_data[i]]
+    c.close()
+
+
+def test_coroutine_process():
+    manager = Manager()
+    q = manager.Queue()
+
+    p = Pool(process_count)
+    for i in range(0, process_count):
+        p.apply_async(coroutine_process_func, args=(q, process_data[i]))
+    p.close()
+    p.join()
+
+    print('total: %d' % q.qsize())
+    while not q.empty():
+        print(q.get())
+
+
+def coroutine_process_func(q, data):
+    c = coroutine_consumer(q)
+    coroutine_process_produce(c, data)
+
+
+def coroutine_process_produce(c, data):
+    a = c.send(None)
+    [c.send(a(i)) for i in data]
+    c.close()
+
+
+def test_gevent():
+    q = Queue()
+    gevent.joinall([gevent.spawn(test_gevent_get_page, q, url)
+                    for url in url_list])
+    print('total: %d' % q.qsize())
+    while not q.empty():
+        print(q.get())
+
+
+def test_gevent_get_page(q, url):
+    return q.put(url)
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    q.put(soup.select("#content_left .result.c-container h3")
+          [0].get_text().strip())
+
+
 if __name__ == '__main__':
     # 多进程
     # test_process()
@@ -88,4 +162,13 @@ if __name__ == '__main__':
     # test_pool_process()
 
     # 多线程
-    test_thread()
+    # test_thread()
+
+    # 协程
+    # test_coroutine()
+
+    # 协程，gevent方式
+    # test_gevent()
+
+    # 多进程+协程
+    # test_coroutine_process()
