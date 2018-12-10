44cd0f0b4a9c3ffa7b51341abac16ba755aa25b4
gaoxt <simerpc@gmail.com>
2018-12-10 13:24:25 +0800

    fix daemon is already running

--- 2018/nginx-keepalived/nginx/entrypoint.sh
+++ 2018/nginx-keepalived/nginx/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
-/usr/sbin/keepalived -n -l -D -f /etc/keepalived/keepalived.conf --dont-fork --log-console &
-
-nginx -g "daemon off;"
+#清楚残留的keepalive.pid、vrrp.pid等，不然启动会daemon is already running，无法实现故障移回(failback)，坑，找了我一个下午。
+rm -rf /var/run/*
+/usr/sbin/keepalived -n -l -D -f /etc/keepalived/keepalived.conf --dont-fork --log-console -P &
+nginx -g "daemon off;"  
