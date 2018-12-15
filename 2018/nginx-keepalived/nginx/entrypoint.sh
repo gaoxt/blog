515688f9438cea94701478c08cd4569bf0bcba6f
gaoxt <simerpc@gmail.com>
2018-12-15 09:09:44 +0800

    change dir name

--- 2018/nginx-keepalived/nginx/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#清楚残留的keepalive.pid、vrrp.pid等，不然启动会daemon is already running，无法实现故障移回，坑，找了我一个下午。
-rm -rf /var/run/*
-/usr/sbin/keepalived -n -l -D -f /etc/keepalived/keepalived.conf --dont-fork --log-console -P &
-nginx -g "daemon off;"  
