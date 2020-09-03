9a32e514f2f9fa0155eb7eed8a87fc751e25becc
gaoxt <simerpc@gmail.com>
2020-09-03 17:44:43 +0800

    add 02.c

--- 2020/PTA/02.c
+++ 2020/PTA/02.c
@@ -18,8 +18,10 @@ List Merge(List L1, List L2);
 int main()
 {
     List L1, L2, L;
-    L1 = Read();
-    L2 = Read();
+    int arr[] = {1, 3, 5};
+    L1 = Read(arr, 3);
+    int arr1[] = {2, 4, 6, 8, 10};
+    L2 = Read(arr1, 5);
     L = Merge(L1, L2);
     Print(L);
     Print(L1);
@@ -28,3 +30,61 @@ int main()
 }
 
 /* 你的代码将被嵌在这里 */
+List Read(int arr[], int n)
+{
+    List L = (List)malloc(sizeof(PtrToNode));
+    L->Next = NULL;
+    if (n)
+    {
+        List r = L; //通过r记录表尾所在位置
+        for (int i = 0; i < n; i++)
+        {
+            List s = (List)malloc(sizeof(struct Node));
+            s->Data = arr[i];
+            r->Next = s; //尾插法
+            r = s;       //更新最新的表尾为s
+        }
+        r->Next = NULL;
+    }
+    return L;
+}
+
+void Print(List L)
+{
+    if (!L->Next)
+        printf("NULL");
+    while (L->Next)
+    {
+        L = L->Next;
+        printf("%d ", L->Data);
+    }
+    printf("\n");
+}
+
+List Merge(List L1, List L2)
+{
+    List pa, pb, pc, L;
+    L = (List)malloc(sizeof(struct Node));
+    pa = L1->Next;
+    pb = L2->Next;
+    pc = L;
+    while (pa && pb)
+    {
+        if (pa->Data <= pb->Data)
+        {
+            pc->Next = pa;
+            pc = pa;
+            pa = pa->Next;
+        }
+        else
+        {
+            pc->Next = pb;
+            pc = pb;
+            pb = pb->Next;
+        }
+    }
+    pc->Next = pa ? pa : pb;
+    L1->Next = NULL;
+    L2->Next = NULL;
+    return L;
+}
--- 2020/PTA/README.md
+++ 2020/PTA/README.md
@@ -3,5 +3,3 @@
 
 # Description
 [中国大学MOOC-陈越、何钦铭-数据结构-2020夏](http://www.icourse163.org/course/ZJU-93001)
-
-PS: 一杯茶一包烟 一道链表写一天
